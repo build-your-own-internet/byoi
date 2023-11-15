0a494be700d04955c01b8e7bd387ae8c94274470
Peter Banka <peter.banka@gmail.com>
2023-11-15 15:40:00 -0800

    ✨ Build out network for name-resolution chaper 1
    - update network map
    - add routes
    - create docker-compose
    
    Co-authored-by: Squee Mueller <smueller@fastly.com>

--- future/name-resolution/001-nr-getting-started/init/start-up.sh
+++ future/name-resolution/001-nr-getting-started/init/start-up.sh
@@ -3,42 +3,57 @@
 /usr/sbin/ip route delete default
 
 case $HOSTNAME in
-  (server) 
-    ip route add default via 5.0.1.1 
+  (host-a) 
+    ip route add default via 1.0.1.1 
     ;;
-  (client)
-    ip route add default via 1.0.5.1 
+  (host-b) 
+    ip route add default via 5.0.2.1 
+    ;;
+  (host-c) 
+    ip route add default via 6.0.3.1 
+    ;;
+  (host-d) 
+    ip route add default via 7.0.4.1 
+    ;;
+  (host-e) 
+    ip route add default via 8.0.5.1 
+    ;;
+  (host-f) 
+    ip route add default via 6.0.3.1 
+    ;;
+  (host-g) 
+    ip route add default via 2.0.6.1 
     ;;
   (router1)
-    ip route add 1.0.0.0/8 via 3.0.3.1
-    ip route add 100.1.0.0/16 via 3.0.3.1
-    ip route add 200.1.1.0/29 via 200.1.1.11
-    ip route add 200.1.1.16/29 via 200.1.1.11
+    ip route add default via 9.0.2.1
+    ip route add 2.0.0.0/8 via 3.0.6.1
+    ip route add 8.0.0.0/8 via 3.0.5.1
     ;;
   (router2)
-    ip route add 1.0.0.0/8 via 100.1.5.1
-    ip route add 3.0.0.0/8 via 100.1.3.1
-    ip route add 5.0.0.0/8 via 200.1.1.10
-    ip route add 200.1.1.16/29 via 200.1.1.3
+    ip route add default via 4.0.4.1
+    ip route add 1.0.0.0/8 via 9.0.1.1
+    ip route add 6.0.0.0/8 via 4.0.3.1
     ;;
   (router3)
-    ip route add 5.0.0.0/8 via 3.0.1.1
-    ip route add 1.0.0.0/8 via 100.1.5.1
-    ip route add 200.1.1.8/29 via 3.0.1.1
-    ip route add 200.1.1.0/29 via 100.1.2.1
-    ip route add 200.1.1.16/29 via 100.1.4.1
+    ip route add default via 4.0.4.1
+    ip route add 5.0.0.0/8 via 4.0.2.1
+    ip route add 1.0.0.0/8 via 4.0.2.1
     ;;
   (router4)
-    ip route add 1.0.0.0/8 via 100.1.5.1
-    ip route add 3.0.0.0/8 via 100.1.3.1
-    ip route add 5.0.0.0/8 via 100.1.3.1
-    ip route add 200.1.1.8/29 via 200.1.1.2
+    ip route add default via 10.0.5.1
+    ip route add 6.0.0.0/8 via 4.0.3.1
+    ip route add 5.0.0.0/8 via 4.0.2.1
+    ip route add 1.0.0.0/8 via 4.0.2.1
     ;;
   (router5)
-    ip route add 3.0.0.0/8 via 100.1.3.1
-    ip route add 5.0.0.0/8 via 200.1.1.18
-    ip route add 200.1.1.8/29 via 100.1.2.1
-    ip route add 200.1.1.0/29 via 200.1.1.18
+    ip route add default via 10.0.4.1
+    ip route add 2.0.0.0/8 via 3.0.6.1
+    ip route add 1.0.0.0/8 via 3.0.1.1
+    ;;
+  (router6)
+    ip route add default via 3.0.5.1
+    ip route add 1.0.0.0/8 via 3.0.1.1
+    ip route add 5.0.0.0/8 via 3.0.1.1
     ;;
 esac
 
