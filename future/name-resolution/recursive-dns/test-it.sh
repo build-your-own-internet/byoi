589c8e92c70c2ad9dcf29bcd2c0d3142d1050e8b
Squee Mueller <squeemishly@gmail.com>
2024-09-20 17:25:10 -0600

    add name resolution for machines on telia network

--- future/name-resolution/recursive-dns/test-it.sh
+++ future/name-resolution/recursive-dns/test-it.sh
@@ -5,13 +5,6 @@ set -e
 echo "Testing name resolution for i2-a.isc.org from client-c1..."
 docker exec build-your-own-internet-client-c1 ping i2-2-8.isc.org -c1
 echo 'DONE!'
-# from client-s2, ping i2-b.isc.org
-
-# from server-g1, ping i2-c.isc.org
-# from server-a1, ping i2-a.isc.org
-#
-#
-# Let's add c2.comcast.com for
 
 echo "Testing name resolution for comcast c2 router from client-s2..."
 docker exec build-your-own-internet-client-s2 ping c2-1-2.comcast.com -c1
@@ -34,6 +27,13 @@ docker exec build-your-own-internet-server-a1 ping z5-2-5-6.zayo.net -c1
 docker exec build-your-own-internet-server-a1 ping z6-2-6-8.zayo.net -c1
 docker exec build-your-own-internet-server-a1 ping z7-2-7-8.zayo.net -c1
 docker exec build-your-own-internet-server-a1 ping z8-2-8.zayo.net -c1
+echo 'DONE!'
+
+echo "Testing name resolution for telia routers from client-c2..."
+docker exec build-your-own-internet-client-c2 ping t5-3-5-7.telia.net -c1
+docker exec build-your-own-internet-client-c2 ping t6-3-6-8.telia.net -c1
+docker exec build-your-own-internet-client-c2 ping t7-3-4.telia.net -c1
+docker exec build-your-own-internet-client-c2 ping t8-3-9.telia.net -c1
 echo 'DONE!'
 
 # TODO: MAke this loudly fail
