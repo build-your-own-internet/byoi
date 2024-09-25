b89267da6ab031684ccc5a82097bd78e397259b0
Peter Banka <psbanka@users.noreply.github.com>
2024-09-25 11:53:37 -0700

    ✨ Add full name-resolution testing
    - also fix an error in the google zone
    - add interrupt for test-it.sh so ^C is respected
    - also fix a bad IP address in ip-addresses
    
    Co-authored-by: Ashish Dixit <59220+tundal45@users.noreply.github.com>
    Co-authored-by: Squee Mueller <squeemishly@gmail.com>

--- future/name-resolution/recursive-dns/test-it.sh
+++ future/name-resolution/recursive-dns/test-it.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 # Testing script
 
+cleanup() {
+    echo "ABORTING..."
+    # Perform cleanup tasks (remove temp files, etc.)
+    exit 1
+}
+
+trap cleanup INT  # Trap the INT signal (Ctrl+C)
+
+
 # Initialize an error counter
 error_count=0
-
 
 # ding is docker ping because ping most likely is already installed locally
 function ding() {
   local source="build-your-own-internet-$1"
   local destination=$2
   echo "Testing connectivity from $1 to $destination"
-  docker exec $source ping -c 1 $destination > /dev/null
+  docker exec $source ping -c 1 $destination >/dev/null
   if [ $? -ne 0 ]; then
     echo "🚨 Error: Failed to connect from $1 to $destination"
-    error_count=$((error_count + 1))  # Increment the error counter
+    error_count=$((error_count + 1)) # Increment the error counter
   fi
 }
 
@@ -22,40 +30,20 @@ systems=("client-c1" "server-g3" "server-s1" "server-a1")
 
 # Read the IP addresses from the file
 ip_addresses=$(cat ip-addresses.txt)
+dns_names=$(cat dns-names.txt)
 
-echo "Testing IP connectivity"
 
 # Loop through each system and each IP address to run the command
 for system in "${systems[@]}"; do
+  echo "Testing IP connectivity"
   for ip in $ip_addresses; do
     ding "$system" "$ip"
   done
+  echo "Testing name resolution"
+  for dns_name in $dns_names; do
+    ding "$system" "$dns_name"
+  done
 done
-
-echo "Doing additional stuff"
-
-ding client-c1 i2-2-8.isc.org
-
-ding client-s2 c2-1-2.comcast.com
-ding client-s2 c2-1-2.comcast.com
-ding client-s2 c2-1-3.comcast.com
-
-ding client-c2 v4-2-8.verisign.org
-ding client-c2 v4-102.verisign.org
-
-ding server-g3 n2-2-4.netnod.org
-ding server-g3 n2-3-4.netnod.org
-ding server-g3 n2-101.netnod.org
-
-ding server-a1 z5-2-5-6.zayo.net
-ding server-a1 z6-2-6-8.zayo.net
-ding server-a1 z7-2-7-8.zayo.net
-ding server-a1 z8-2-8.zayo.net
-
-ding client-c2 t5-3-5-7.telia.net
-ding client-c2 t6-3-6-8.telia.net
-ding client-c2 t7-3-4.telia.net
-ding client-c2 t8-3-9.telia.net
 
 # Summary of errors
 if [ $error_count -eq 0 ]; then
@@ -63,3 +51,4 @@ if [ $error_count -eq 0 ]; then
 else
   echo "⚠️  $error_count errors encountered."
 fi
+
