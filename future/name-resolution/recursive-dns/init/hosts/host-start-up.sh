4b490a0d9e7bb539c3847778689d91760705527d
Squee Mueller <squeemishly@gmail.com>
2024-10-05 14:36:06 -0600

    fix: add tlddns-a to start-routes
    Also, fix weird no-open-parantheses thing... :|
    And add some content to the knot.conf file.
    
    Co-authored-by: Peter Banka <psbanka@users.noreply.github.com>

--- future/name-resolution/recursive-dns/init/hosts/host-start-up.sh
+++ future/name-resolution/recursive-dns/init/hosts/host-start-up.sh
@@ -7,58 +7,55 @@ rm -rf /init
 
 case $HOSTNAME in
 # Comcast network machines
-client-c1)
+  (client-c1)
   ip route add default via 1.1.0.3
   ;;
-client-c2)
+  (client-c2)
   ip route add default via 1.3.0.2
   ;;
 
 # Supercorp networks
-server-s1)
+  (server-s1)
   ip route add default via 9.3.0.2
   ;;
-client-s2)
+  (client-s2)
   ip route add default via 9.2.0.2
   ip route add 9.1.0.0/16 via 9.2.0.3
   ;;
 
 # Google Cloud Services
-server-g1)
+  (server-g1)
   ip route add default via 8.2.0.3
   ip route add 8.1.0.0/16 via 8.2.0.2
   ;;
-server-g2)
+  (server-g2)
   ip route add default via 8.2.0.3
   ip route add 8.1.0.0/16 via 8.2.0.2
   ;;
-server-g3)
+  (server-g3)
   ip route add default via 8.2.0.3
   ip route add 8.1.0.0/16 via 8.2.0.2
   ;;
 
 # AWS
-server-a1)
+  (server-a1)
   ip route add default via 4.2.0.2
   ;;
-server-a2)
+  (server-a2)
   ip route add default via 4.2.0.2
   ;;
-server-a3)
+  (server-a3)
   ip route add default via 4.1.0.4
   ip route add 4.2.0.0./16 via 4.1.0.2
   ip route add 4.3.0.0./16 via 4.1.0.3
   ip route add 9.0.0.0/8 via 4.1.0.3
   ip route add 3.0.0.0/8 via 4.1.0.3
   ;;
-server-a4)
-  ip route add default via 4.3.0.3
-  ;;
 esac
 
-# if [[ $(hostname) =~ server.* ]]; then
+  # if [[ $(hostname) =~ server.* ]]; then
 #   # copy in all the image files for each specific host
-#   cp -a /home/www/$(hostname) /var/www
+  #   cp -a /home/www/$(hostname) /var/www
 #   rm -rf /home/www
 # 
 #   # start an http server on each host
