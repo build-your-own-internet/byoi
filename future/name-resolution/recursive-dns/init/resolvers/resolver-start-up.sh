5f14ace71f9b61d34f577ea0bbcc245c5991961d
Squee Mueller <squeemishly@gmail.com>
2024-11-28 13:33:12 -0700

    add ripe network for final exercises

--- future/name-resolution/recursive-dns/init/resolvers/resolver-start-up.sh
+++ future/name-resolution/recursive-dns/init/resolvers/resolver-start-up.sh
@@ -29,6 +29,9 @@ case $HOSTNAME in
     ip route add default via 8.1.0.4
     ip route add 8.2.0.0/16 via 8.1.0.2
     ;;
+    (resolver-r)
+    ip route add default via 103.0.1.4
+    ;;
 esac
 
 /usr/sbin/unbound -d
