1ae7686e4af94c002315f9af63121d9fdcaf6b88
Peter Banka <psbanka@users.noreply.github.com>
2024-12-05 11:59:44 -0800

    ✨ got IP testing for client-c1
    Co-authored-by: Squee Mueller <squeemishly@gmail.com>

--- future/tls/init/dns-servers/dns-start-up.sh
+++ future/tls/init/dns-servers/dns-start-up.sh
@@ -17,31 +17,11 @@ case $HOSTNAME in
     (authoritative-s)
     ip route add default via 9.1.0.3
     ;;
-    (authoritative-a)
-    ip route add default via 4.1.0.4
-    ip route add 4.2.0.0./16 via 4.1.0.2
-    ip route add 4.3.0.0./16 via 4.1.0.3
-    ip route add 9.0.0.0/8 via 4.1.0.3
-    ip route add 3.0.0.0/8 via 4.1.0.3
-    ;;
-    (rootdns-i)
-    ip route add default via 100.0.1.2
-    ;;
-    (tlddns-a)
-    ip route add default via 4.3.0.3
-    ;;
-    (tlddns-v)
-    ip route add default via 102.0.1.4
-    ;;
     (tlddns-n)
     ip route add default via 101.0.1.2
     ;;
     (rootdns-n)
     ip route add default via 101.0.1.2
-    ;;
-    (tlddns-g)
-    ip route add default via 8.2.0.3
-    ip route add 8.1.0.0/16 via 8.2.0.2
     ;;
 esac
 
