4a85825b43eba73e2e51182fd8c480a97fc6eb1b
Peter Banka <pbanka@fastly.com>
2025-11-14 13:08:06 -0800

    get final and init working for byoi-rebuild for 1.1 - 2.1

--- chapters/1.1-routing-getting-started/start-up.sh
+++ chapters/1.1-routing-getting-started/start-up.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
 # REMOVE ALL IP ADDRESSES IN INIT VERSION
-# Get all IP addresses on eth0
-ips=$(ip -o -4 addr show dev eth0 | awk '{print $4}')
-
-# Loop through each IP and remove it
-for ip in $ips; do
-  ip addr del "$ip" dev eth0
-done
 
 /usr/sbin/ip route delete default
 
